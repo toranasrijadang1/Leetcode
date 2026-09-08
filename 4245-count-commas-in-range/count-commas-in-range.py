b8a71027205ba80b71a77f46799dadf14e1daa22
TORANA SRIJA DANGETI <toranasrijadangeti2008@gmail.com>
2026-09-09 01:09:34 +0530

    Time: 225 ms (17.83%) | Memory: 19.2 MB (86.30%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        c = 0
+        if n < 1000:
+            return 0
+        if n >= 1000:
+            for i in range(1000,n+1):
+                c = c + 1
+        return c
+            
