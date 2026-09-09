a92c00f2b232131c2e2895d6b3131025370c2670
TORANA SRIJA DANGETI <toranasrijadangeti2008@gmail.com>
2026-09-09 11:01:16 +0530

    Time: 162 ms (78.45%) | Memory: 20.7 MB (44.26%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,16 @@
+def caneat(piles, hours_have, k):
+    hours_needed = 0
+    for pile in piles:
+        hours_needed += math.ceil(pile/k)
+    return hours_needed <= hours_have
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:  
+        low = 1
+        high = max(piles)
+        while low < high:
+            mid = (low + high)//2
+            if caneat(piles,h,mid):
+                high = mid
+            else:
+                low = mid + 1
+        return low        
